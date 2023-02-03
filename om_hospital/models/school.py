4cb350f0605d03f1aec684db75bbded95dd89bb6
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-03 14:36:19 +0200

    Add files via upload

--- om_hospital/models/school.py
+++ om_hospital/models/school.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import date
 from odoo import fields, models, api
 
 
@@ -14,7 +14,7 @@ class SchoolProfile(models.Model):
     gender_type = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
     date_of_birth = fields.Date(string='Date_of_Birth')
     is_teenager =fields.Char(string="Teenager?",compute='_compute_is_teenager')
-    age_status =fields.Char("Age Status",compute='_compute_age_status')
+
     @api.depends('age')
     def _compute_is_teenager(self):
         for record in self :
@@ -22,13 +22,10 @@ class SchoolProfile(models.Model):
                  record.is_teenager ='Teenager'
             else:
                  record.is_teenager='Not a Teenager'
-    def _compute_age_status(self):
-        for record in self:
-            age= (datetime.now()- record.date_of_birth).days// 365
-            if 20 <= age <= 50:
-                record.age_status = "Adult"
-            else:
-                record.age_status="Not Adult"
+        elif record.age>=20 and record.age<=50:
+                record.is_teenager='Adult'
+        else:
+               record.is_teenager='5etyar'
 
 
 
