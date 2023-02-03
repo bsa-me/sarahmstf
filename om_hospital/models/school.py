a7ec5bce67506d51601864f6aafe0c5fc703af65
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-03 12:27:23 +0200

    Add files via upload

--- om_hospital/models/school.py
+++ om_hospital/models/school.py
@@ -8,8 +8,8 @@ class SchoolProfile(models.Model):
     email = fields.Char(string="Email")
     phone = fields.Char("phone")
     age = fields.Integer("age")
-    open_date= fields.datetime("Open Date")
-    school_type=fields.selection([('public',public School'),('private',private School')],string="Type of School,")
+    open_date= fields.Datetime("Open Date")
+    gender_type=fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
 
 
 
