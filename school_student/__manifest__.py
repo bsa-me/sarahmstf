680a3ce29a5ddf5d4c1f3e335e539eea29458e55
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-07 12:21:55 +0200

    Add files via upload

---
+++ school_student/__manifest__.py
@@ -0,0 +1,22 @@
+#-*-coding: utf-8-*-
+{
+    'name': 'Hospital Management',
+    'Version': '1.0.0',
+    'summary': 'Hospital Management software',
+    'sequence': 100,
+    'description': """Hospital Management software""",
+    'category': 'productivity',
+    'website': "https://www.odoomates.tech",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sara.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installation': True,
+    'application': True,
+    'auto_install': False,
+    'licence': 'LGPL-3',
+}
+
