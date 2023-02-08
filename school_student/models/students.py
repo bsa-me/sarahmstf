06dcb73315db5d523057e9ef33069c6f403c241a
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-08 14:48:27 +0200

    Add files via upload

--- school_student/models/students.py
+++ school_student/models/students.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 
 
 class school_student(models.Model):
+
     _name = "schools.student"
     _description = "schools_student.school_student"
 
@@ -9,6 +10,7 @@ class school_student(models.Model):
     schools_id = fields.Many2one("school.profile", string="Schools Name")
     phone = fields.Char("phone")
     stage = fields.Selection([('pending','Pending'),('approved','Approved'),('sold','Sold'),('cancelled','Cancelled'),],default='pending',string='Stage')
+    child_ids = fields.One2many('child.model', 'parent_id', string='Children')
 
     def approve_button(self):
         self.ensure_one()
@@ -22,16 +24,16 @@ class school_student(models.Model):
         self.ensure_one()
         self.stage = 'cancelled'
 
-   class ChildModel(models.Model):
-   _name = 'child.model'
+
+class ChildModel(models.Model):
+   _name = 'schools.model'
 
    name = fields.Char(string='name')
    price = fields.Float(string='price')
    number = fields.Integer(string='Number')
-   parent_id = fields.Many2one('parent.model',string='parent')
+   parent_id = fields.Many2one('schools.student',string='parent')
 
-   class ParentModel(models.Model):
-       _name ='parent.model'
-
-       name = field.Char(string='name')
-       child_ids = fields.One2many('child.model','parent_id',string='Children')
+   # class ParentModel(models.Model):
+   #     _name ='parent.model'
+   #
+   #     name = field.Char(string='name')
