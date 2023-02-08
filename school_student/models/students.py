f67fd02a3ed6a7d8199a06e3d5b6e73a1545186f
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-08 14:24:36 +0200

    Add files via upload

--- school_student/models/students.py
+++ school_student/models/students.py
@@ -21,3 +21,17 @@ class school_student(models.Model):
     def cancel_button(self):
         self.ensure_one()
         self.stage = 'cancelled'
+
+   class ChildModel(models.Model):
+   _name = 'child.model'
+
+   name = fields.Char(string='name')
+   price = fields.Float(string='price')
+   number = fields.Integer(string='Number')
+   parent_id = fields.Many2one('parent.model',string='parent')
+
+   class ParentModel(models.Model):
+       _name ='parent.model'
+
+       name = field.Char(string='name')
+       child_ids = fields.One2many('child.model','parent_id',string='Children')
