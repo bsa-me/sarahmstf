344021251fa0213d0aac46552f34d90cdc8f1fa9
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-08 13:13:49 +0200

    Add files via upload

--- school_student/models/students.py
+++ school_student/models/students.py
@@ -1,19 +1,23 @@
 from odoo import models, fields, api
 
-class school_student(models.Model):
-    _name="schools.student"
-    _description= "schools_student.school_student"
 
-    name=fields.Char()
-    schools_id=fields.Many2one("school.profile", string="Schools Name")
-    phone=fields.Char("phone")
-    stage= fields.Selection([('pending','Pending'),('approved','Approved'),('sold','Sold'),('cancelled','Cancelled'),],default='pending',string='Stage')
+class school_student(models.Model):
+    _name = "schools.student"
+    _description = "schools_student.school_student"
+
+    name = fields.Char()
+    schools_id = fields.Many2one("school.profile", string="Schools Name")
+    phone = fields.Char("phone")
+    stage = fields.Selection([('pending','Pending'),('approved','Approved'),('sold','Sold'),('cancelled','Cancelled'),],default='pending',string='Stage')
+
     def approve_button(self):
         self.ensure_one()
         self.stage='approved'
+
     def sell_button(self):
         self.ensure_one()
         self.stage = 'sold'
+
     def cancel_button(self):
         self.ensure_one()
         self.stage = 'cancelled'
