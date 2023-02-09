f4b5abd951e577ad6570b7e5a0401a840233e9ad
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-09 15:49:11 +0200

    Add files via upload

--- school_student/models/students.py
+++ school_student/models/students.py
@@ -12,13 +12,14 @@ class school_student(models.Model):
     stage = fields.Selection([('pending','Pending'),('approved','Approved'),('sold','Sold'),('cancelled','Cancelled'),],default='pending',string='Stage')
     child_ids = fields.One2many('schools.model', 'parent_id', string='Checkbox')
     total_price = fields.Float(string='Total Price')
-    division = fields.Float(string='Division')
+    division = fields.Integer(string='Division')
 
     def approve_button(self):
         self.ensure_one()
         self.stage = 'approved'
+        self.write({'stage':'approved'})
         for record in self:
-            for i in range(record.division):
+            for i in range(int(record.division)):
                 division_amount = record.total_price / record.division
                 record.child_ids.create({'amount': division_amount,
                                      'parent_id': record.id})
@@ -27,10 +28,13 @@ class school_student(models.Model):
     def sell_button(self):
         self.ensure_one()
         self.stage = 'sold'
+        self.is_selected = True
 
     def cancel_button(self):
         self.ensure_one()
         self.stage = 'cancelled'
+        for i in self.child_ids:
+        line.unlink()
 
 
 class ChildModel(models.Model):
