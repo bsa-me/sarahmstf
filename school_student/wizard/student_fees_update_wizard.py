1439e7d8204a155c5a45e9d50bc551c093185fcb
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-13 12:19:36 +0200

    Add files via upload

--- school_student/wizard/student_fees_update_wizard.py
+++ school_student/wizard/student_fees_update_wizard.py
@@ -12,5 +12,5 @@ class StudentFeesUpdateWizard(models.TransientModel):
         for record in self:
             for i in range(int(record.division)):
         division_price = self.price / self.division
-        distribution_obj = self.env['distribution.model']
+        distribution_obj = self.env['schools.model']
         distribution = distribution_obj.create({'amount':division_price})
