360141ffee00b0e1011b0eccff5122aeace08ed9
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-13 16:00:59 +0200

    Add files via upload

--- game_name/models/game.py
+++ game_name/models/game.py
@@ -1,7 +1,13 @@
 from odoo import fields, models, api
 
 
+
 class Gamenew(models.Model):
     _name ="game.new"
 
     name = fields.Char()
+
+class SaleOrder(models.Model):
+   _inherit = 'sale.order'
+
+   custom_field = fields.Char(string='Custom Field')
