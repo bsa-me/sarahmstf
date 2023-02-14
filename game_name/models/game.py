4cf8663fec964650228e5ba623a84d4e93c4d4e7
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-14 12:28:41 +0200

    Add files via upload

--- game_name/models/game.py
+++ game_name/models/game.py
@@ -5,3 +5,12 @@ class SaleOrder(models.Model):
    _inherit = 'sale.order'
 
    custom_field = fields.Char(string='Custom Field')
+
+class PurshaseOrder(models.Model) :
+   _inherit = 'purshase.order'
+
+   organic = fields.Char(string='Organic')
+
+   def link_sale_order(self, purshase_order_id, sale_order_id):
+      purshase_order = self.browse(purshase_order_id)
+      purshase_order.write({'sale_order_id': sale_order_id})
