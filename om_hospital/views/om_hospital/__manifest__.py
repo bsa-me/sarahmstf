c3011609bf8bb59cb1a2087f22518c12f3b4251f
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-16 13:20:12 +0200

    Add files via upload

---
+++ om_hospital/views/om_hospital/__manifest__.py
@@ -0,0 +1,24 @@
+# -*-coding: utf-8-*-
+
+{
+    'name': 'Hospital Management',
+    'Version': '1.0.0',
+    'summary': 'Hospital Management software',
+    'sequence': 100,
+    'description': """Hospital Management software""",
+    'category': 'productivity',
+    'website': "https://www.odoomates.tech",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sara.xml',
+       # 'reports/report.xml',
+        'reports/school_profile.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installation': True,
+    'application': True,
+    'auto_install': False,
+    'licence': 'LGPL-3',
+}
---
+++ om_hospital/views/om_hospital/models/school.py
@@ -0,0 +1,53 @@
+from datetime import date
+from odoo import fields, models, api
+
+
+class SchoolProfile(models.Model):
+    _name = "school.profile"
+    name = fields.Char(string="school Name")
+    email = fields.Char(string="Email")
+    phone = fields.Char("phone")
+    age = fields.Integer(string="age")
+    open_date = fields.Datetime("Open Date")
+    gender_type = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    date_of_birth = fields.Date(string='Date_of_Birth')
+    is_teenager = fields.Char(string="Teenager?", compute='_compute_is_teenager')
+    is_virtual_class = fields.Boolean(string="Virtual Class Support?", readonly="True")
+
+    school_rank = fields.Selection([('public','Public School'), ('private','Private School')], string="Type of school")
+    result = fields.Float(compute='_compute_result',string="result")
+    address = fields.Text(string="Address")
+    documents = fields.Binary(string="Documents")
+    document_name = fields.Char(string="File Name")
+    currency_id = fields.Many2one("res.currency", string="currency")
+    student_fees = fields.Monetary(string="Student Fees")
+    total_fees = fields.Float(string="Total Fees")
+
+    # def your_button_action(self):
+    #     self.write({'your_button_action"':'done with this'})
+
+    @api.depends('age')
+    def _compute_is_teenager(self):
+        for record in self:
+            if record.age < 20:
+                record.is_teenager = 'Teenager'
+
+            elif record.age >= 20 and record.age <= 50:
+                record.is_teenager = 'Adult'
+            else:
+                record.is_teenager = '5etyar'
+
+    def your_button_action(self):
+        print("hi")
+
+    @api.depends('school_rank')
+    def _compute_result(self):
+        for record in self:
+            if record.school_rank =="private":
+                record.result = 50
+            elif record.school_rank =="public":
+                record.result = 100
+            else:
+                record.result = 0
+
+
