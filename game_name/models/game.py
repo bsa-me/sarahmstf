e614c20f29e0609f453bc3ad984ea85b3bb35807
saramstfa4 <123943852+saramstfa4@users.noreply.github.com>
2023-02-27 19:48:44 +0200

    Add files via upload

--- game_name/models/game.py
+++ game_name/models/game.py
@@ -5,13 +5,19 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     custom_field = fields.Char(string='Custom Field')
-    somme = fields.Float(string='somme', compute='_compute_somme')
+    somme = fields.Float(string='Somme', compute='_compute_somme')
+    saleordercount = fields.Integer(string='Sale Order Count', compute='_compute_saleordercount')
+    partnerid = fields.Many2one('res.partner', string='partner')
+
 
     def _compute_somme(self):
         for order in self:
             order.somme = sum(line.product_uom_qty for line in order.order_line)
 
-
+    @api.depends('order_line')
+   def _compute_saleordercount(self) :
+    for record in self:
+        record.saleordercount =len(record.order_line)
 
 
 class PurchaseOrder(models.Model):
@@ -20,5 +26,4 @@ class PurchaseOrder(models.Model):
     sale_order_id = fields.Many2one('sale.order',string='Sale Order')
 
     game_field = fields.Char(string='Game')
-    
 
